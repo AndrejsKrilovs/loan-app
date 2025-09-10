edcf24d4b38abdb1479d3571a8a90d50d08a9b77
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2025-09-10 19:55:52 +0300

    Implemented code style formatting and add spring data jpa and postgresql database

--- src/main/java/krilovs/andrejs/app/user/UserEntity.java
+++ src/main/java/krilovs/andrejs/app/user/UserEntity.java
@@ -1,16 +1,66 @@
 package krilovs.andrejs.app.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
+@Entity
 @ToString(exclude = {"password"})
+@Table(
+  name = "user_table",
+  indexes = {
+    @Index(
+      name = "user_email_ind",
+      columnList = "user_email",
+      unique = true
+    )
+  }
+)
 public class UserEntity {
+  @Id
+  @Column(name = "user_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(
+    name = "user_email",
+    unique = true,
+    nullable = false,
+    length = 30
+  )
   private String email;
+
+  @Column(
+    name = "user_password",
+    nullable = false
+  )
   private String password;
+
+  @ColumnDefault("'CUSTOMER'")
+  @Enumerated(EnumType.STRING)
+  @Column(
+    name = "user_role",
+    nullable = false,
+    length = 10
+  )
   private UserRole role;
+
+  @ColumnDefault("true")
+  @Column(
+    name = "is_user_enabled",
+    nullable = false
+  )
   private Boolean enabled;
 }
