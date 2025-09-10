cc89c550ee33ccae5ba604d71c8244e17b48ba95
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2025-09-10 23:55:57 +0300

    Solved n+1 problem for ActiveUserRepository.findAll

--- src/main/java/krilovs/andrejs/app/user/active/ActiveUserRepository.java
+++ src/main/java/krilovs/andrejs/app/user/active/ActiveUserRepository.java
@@ -1,11 +1,15 @@
 package krilovs.andrejs.app.user.active;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ActiveUserRepository extends JpaRepository<ActiveUserEntity, Long> {
@@ -19,4 +23,9 @@ public interface ActiveUserRepository extends JpaRepository<ActiveUserEntity, Lo
     nativeQuery = true
   )
   void add(@Param("userId") Long userId);
+
+  @NonNull
+  @Override
+  @EntityGraph(attributePaths = {"user"})
+  List<ActiveUserEntity> findAll();
 }
